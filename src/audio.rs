327b0f17c8ccff9314edaabfd291e89a307b2543
soeguet <soeguet.osman@gmail.com>
2025-08-03 15:11:50 +0200

    add: styling and settings, windows resizing

--- src/audio.rs
+++ src/audio.rs
@@ -2,18 +2,52 @@ use kira::sound::PlaybackState;
 use kira::{
     AudioManager, AudioManagerSettings, DefaultBackend, sound::static_sound::StaticSoundData,
 };
+use std::fmt;
+
+#[derive(Debug)]
+pub struct AudioError {
+    pub message: String,
+}
+impl fmt::Display for AudioError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "AudioError: {}", self.message)
+    }
+}
+
+impl std::error::Error for AudioError {}
+
+pub struct CustomAudio {
+    pub audio_path: String,
+    pub audio_path_int: String,
+    audio_data: StaticSoundData,
+}
 
 pub struct AudioMaster {
-    audio_data: StaticSoundData,
+    pub(crate) main_audio: CustomAudio,
+    break_audio: CustomAudio,
 }
 
 impl Default for AudioMaster {
     fn default() -> Self {
-        let sound_data =
-            StaticSoundData::from_file("public/happy-message-ping-351298.mp3").unwrap();
+        let init_audio_path = "public/happy-message-ping-351298.mp3".to_owned();
+        let sound_data = StaticSoundData::from_file(&init_audio_path).unwrap();
+        let main_audio = CustomAudio {
+            audio_data: sound_data,
+            audio_path_int: String::new(),
+            audio_path: init_audio_path,
+        };
+
+        let init_audio_path = "public/happy-message-ping-351298.mp3".to_owned();
+        let sound_data = StaticSoundData::from_file(&init_audio_path).unwrap();
+        let break_audio = CustomAudio {
+            audio_data: sound_data,
+            audio_path_int: String::new(),
+            audio_path: init_audio_path,
+        };
 
         Self {
-            audio_data: sound_data,
+            main_audio,
+            break_audio,
         }
     }
 }
@@ -24,13 +58,32 @@ impl AudioMaster {
         let mut manager =
             AudioManager::<DefaultBackend>::new(AudioManagerSettings::default()).unwrap();
         let result = manager
-            .play(self.audio_data.clone())
+            .play(self.main_audio.audio_data.clone())
             .expect("Could not load audio");
 
         loop {
             if result.state() != PlaybackState::Playing {
                 break;
             }
+        }
+    }
+
+    pub fn pre_settings_phase(&mut self) {
+        self.main_audio.audio_path_int = self.main_audio.audio_path.clone();
+    }
+
+    pub fn set_main_audio_path(&mut self, new_audio_path: String) -> Result<(), AudioError> {
+        let result = StaticSoundData::from_file(&self.main_audio.audio_path);
+
+        match result {
+            Ok(n) => {
+                self.main_audio.audio_data = n;
+                self.main_audio.audio_path = new_audio_path;
+                Ok(())
+            }
+            Err(err) => Err(AudioError {
+                message: format!("could not load audio file: \"{}\"", new_audio_path),
+            }),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,28 @@ mod audio;
 
 use crate::audio::AudioMaster;
 use eframe::egui;
-use egui::{Color32, ProgressBar};
+use egui::{Color32, ProgressBar, Ui, Vec2, ViewportCommand};
 use std::time::{Duration, Instant};
+
+const TICKER_SCREEN_WIDTH: f32 = 74.0;
+const TICKER_SCREEN_HEIGHT: f32 = 200.0;
+const SETTINGS_SCREEN_WIDTH: f32 = 500.0;
+const SETTINGS_SCREEN_HEIGHT: f32 = 500.0;
+const TICKER_START_COUNT: f32 = 0.0;
+const TICKER_INTERVAL: f32 = 1.0;
+const SPACING: f32 = 5.0;
 
 enum State {
     MainState,
     SettingsState,
+    BreakState,
     AudioState,
 }
 
 struct Settings {
     time: u32,
+    break_time: u32,
+    prev_state: State,
 }
 
 struct Progress {
@@ -21,7 +32,9 @@ struct Progress {
 }
 
 struct RustickApp {
+    message: Option<(String, Instant)>,
     state: State,
+    next_state: State,
     settings: Settings,
     progress: Progress,
     audio_master: AudioMaster,
@@ -30,10 +43,16 @@ struct RustickApp {
 impl Default for RustickApp {
     fn default() -> Self {
         Self {
+            message: None,
             state: State::MainState,
-            settings: Settings { time: 10 },
+            next_state: State::BreakState,
+            settings: Settings {
+                time: 10,
+                break_time: 5,
+                prev_state: State::MainState,
+            },
             progress: Progress {
-                progress: 0.0,
+                progress: TICKER_START_COUNT,
                 last_ticker_update: Instant::now(),
             },
             audio_master: AudioMaster::default(),
@@ -43,68 +62,218 @@ impl Default for RustickApp {
 
 impl RustickApp {
     fn update_progress(&mut self) {
-        self.progress.progress += 1.0
+        self.progress.progress += TICKER_INTERVAL;
     }
 
-    fn calculate_current_progress(&mut self) -> f32 {
-        (self.progress.progress / self.settings.time as f32 * 100.0).floor() / 100.0
+    fn render_go_to_settings_button(
+        &mut self,
+        ui: &mut Ui,
+        ctx: &egui::Context,
+        prev_state: State,
+    ) {
+        if ui.button("Go to Settings").clicked() {
+            self.audio_master.pre_settings_phase();
+            self.state = State::SettingsState;
+            self.settings.prev_state = prev_state;
+
+            let size: Vec2 = Vec2 {
+                x: SETTINGS_SCREEN_WIDTH,
+                y: SETTINGS_SCREEN_HEIGHT,
+            };
+            ctx.send_viewport_cmd(ViewportCommand::InnerSize(size));
+        }
     }
-}
 
-impl eframe::App for RustickApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Looks better on 4k montior
-        ctx.set_pixels_per_point(1.5);
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            let formatted_label = format!("{}/{}", self.progress.progress, self.settings.time);
-            ui.label(formatted_label);
-
-            let progress_bar = ProgressBar::new(self.calculate_current_progress())
-                .fill(Color32::BLUE)
-                .show_percentage();
-
-            ui.add(progress_bar);
-
-            if ui.button("Go to Settings").clicked() {
-                self.state = State::SettingsState;
-            }
-
-            if let State::SettingsState = self.state {
-                ui.label("Settings");
-                ui.add(egui::Slider::new(&mut self.settings.time, 0..=180).text("Duration"));
-                if ui.button("Back to Main Screen").clicked() {
-                    self.state = State::MainState;
-                }
-            }
-        });
-
+    fn calculate_current_progress(&mut self, time: f32) -> f32 {
+        (self.progress.progress / time * 100.0).floor() / 100.0
+    }
+    fn evaluate_progress(&mut self) {
         if self.progress.last_ticker_update.elapsed() > Duration::from_millis(950) {
             self.update_progress();
             self.progress.last_ticker_update = Instant::now();
         };
+    }
+
+    fn render_settings_screen(&mut self, ctx: &egui::Context) {
+        egui::CentralPanel::default().show(&ctx, |ui| {
+            // top settings label
+            ui.label(
+                egui::RichText::new("Settings")
+                    .extra_letter_spacing(2.0)
+                    .size(25.0)
+                    .underline(),
+            );
+
+            // errors
+            if let Some((msg, since)) = &self.message {
+                if since.elapsed().as_secs_f32() < 3.0 {
+                    ui.add_space(SPACING);
+                    ui.separator();
+                    ui.label(
+                        egui::RichText::new(msg.clone())
+                            .color(Color32::RED)
+                            .underline(),
+                    );
+                    ui.separator();
+                    ui.add_space(SPACING);
+                } else {
+                    self.message = None;
+                }
+            }
+
+            // slide main
+            ui.add_space(SPACING);
+            ui.label("Change time - Main");
+            ui.add(egui::Slider::new(&mut self.settings.time, 0..=600).text("Duration"));
+
+            // main audio path
+            ui.add_space(SPACING);
+            ui.label("Change time - Break");
+            ui.add(egui::TextEdit::singleline(
+                &mut self.audio_master.main_audio.audio_path,
+            ));
+            if ui.button("save new audio file").clicked() {
+                if self.audio_master.main_audio.audio_path
+                    != self.audio_master.main_audio.audio_path_int
+                {
+                    match self
+                        .audio_master
+                        .set_main_audio_path(self.audio_master.main_audio.audio_path_int.clone())
+                    {
+                        Ok(_) => {
+                            self.message =
+                                Some(("Changes were applied".to_string(), Instant::now()));
+                        }
+                        Err(err) => {
+                            self.message = Some((format!("{}", err.message), Instant::now()));
+                        }
+                    }
+                } else {
+                    self.message = Some(("The path is the same...".to_string(), Instant::now()));
+                }
+            }
+            ui.add_space(SPACING);
+            ui.separator();
+
+            // slider break
+            ui.add_space(SPACING);
+            ui.label("Change time - Break");
+            ui.add(
+                egui::Slider::new(&mut self.settings.break_time, 0..=180).text("Break Duration"),
+            );
+
+            ui.add_space(SPACING);
+            ui.separator();
+            // back button
+            ui.add_space(3.0 * SPACING);
+            if ui.button("Back to Main Screen").clicked() {
+                match &self.settings.prev_state {
+                    State::MainState => {
+                        self.state = State::MainState;
+                    }
+                    State::BreakState => {
+                        self.state = State::BreakState;
+
+                        self.settings.prev_state = State::MainState;
+                    }
+                    _ => {}
+                };
+
+                let size: Vec2 = Vec2 {
+                    x: TICKER_SCREEN_HEIGHT,
+                    y: TICKER_SCREEN_WIDTH,
+                };
+                ctx.send_viewport_cmd(ViewportCommand::InnerSize(size));
+            }
+        });
+    }
+
+    fn render_main_screen(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(&ctx, |ui| {
+            let formatted_label = format!("{}/{}", self.progress.progress, self.settings.time);
+            ui.label(formatted_label);
+
+            let progress_bar =
+                ProgressBar::new(self.calculate_current_progress(self.settings.time as f32))
+                    .fill(Color32::BLUE)
+                    .show_percentage();
+
+            ui.add(progress_bar);
+
+            self.render_go_to_settings_button(ui, ctx, State::MainState);
+        });
+
+        self.evaluate_progress();
+    }
+
+    fn render_break_screen(&mut self, ctx: &egui::Context) {
+        egui::CentralPanel::default().show(&ctx, |ui| {
+            let formatted_label =
+                format!("{}/{}", self.progress.progress, self.settings.break_time);
+            ui.label(formatted_label);
+
+            let progress_bar =
+                ProgressBar::new(self.calculate_current_progress(self.settings.break_time as f32))
+                    .fill(Color32::DARK_GREEN)
+                    .show_percentage();
+
+            ui.add(progress_bar);
+
+            self.render_go_to_settings_button(ui, ctx, State::BreakState);
+        });
+        self.evaluate_progress();
+    }
+}
+
+impl eframe::App for RustickApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        // Looks better on 4k montior
+        ctx.set_pixels_per_point(1.5);
 
         match self.state {
             State::MainState => {
+                self.render_main_screen(ctx, frame);
+
                 if self.progress.progress > self.settings.time as f32 {
                     self.state = State::AudioState;
-                    self.progress.progress = 0.0;
+                    self.progress.progress = TICKER_START_COUNT;
                 }
             }
-            State::SettingsState => {}
+            State::SettingsState => {
+                self.render_settings_screen(ctx);
+            }
             State::AudioState => {
                 self.audio_master.run_audio();
-                self.state = State::MainState;
+
+                match self.next_state {
+                    State::MainState => {
+                        self.state = State::MainState;
+                        self.next_state = State::BreakState;
+                    }
+                    State::BreakState => {
+                        self.state = State::BreakState;
+                        self.next_state = State::MainState;
+                    }
+                    _ => {}
+                };
+            }
+            State::BreakState => {
+                self.render_break_screen(ctx);
+                if self.progress.progress > self.settings.break_time as f32 {
+                    self.state = State::AudioState;
+                    self.progress.progress = TICKER_START_COUNT;
+                }
             }
         }
 
-        ctx.request_repaint_after_secs(1.0);
+        ctx.request_repaint_after_secs(TICKER_INTERVAL);
     }
 }
 
 fn main() -> eframe::Result<()> {
     let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size((200.0, 100.0)),
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size((TICKER_SCREEN_HEIGHT, TICKER_SCREEN_WIDTH)),
         ..eframe::NativeOptions::default()
     };
 
@@ -121,7 +290,7 @@ mod tests {
         let mut app = RustickApp::default();
         app.progress.progress = 30.0;
         app.settings.time = 60;
-        let progress = app.calculate_current_progress();
+        let progress = app.calculate_current_progress(app.settings.time as f32);
         assert_eq!(progress, 0.50);
     }
     #[test]
@@ -129,7 +298,7 @@ mod tests {
         let mut app = RustickApp::default();
         app.progress.progress = 20.0;
         app.settings.time = 60;
-        let progress = app.calculate_current_progress();
+        let progress = app.calculate_current_progress(app.settings.time as f32);
         assert_eq!(progress, 0.33);
     }
 }
